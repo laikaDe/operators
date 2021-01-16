6fb474cfd257c114df3ea2bc3b3fe07b4f404e77
Laika Deunert <laikadeunert@Laikas-Laptop.local>
2021-01-15 19:13:39 -0800

    commit tree 4c4162b748959b4829474e90c1db3b689e076798

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
+
 def unsafe?(speed)
 
+    if speed > 60
+        true
+    elsif speed < 40 
+        true 
+    else speed == speed.between?(40,60)
+        false
+    end
 end
-
-
 
 def not_safe?(speed)
+
+    speed > 60 or speed < 40 ? true : false
 	
 end
-	
-
-
